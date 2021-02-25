7fedd5cecfd7c4078f2ced783a2b40535de0a0ee
kostayne <fuckogg@yandex.ru>
2021-02-25 18:35:57 +0300

    base function test

---
+++ src/api/create_component.ts
@@ -0,0 +1,3 @@
+export default async function createComponent(name: string) {
+    
+}
---
+++ src/commands/create-component.ts
@@ -0,0 +1,53 @@
+import {Command, flags} from '@oclif/command';
+import { getComponentPath } from "../paths";
+import { validateChooseArg } from '../chooseArg';
+import * as paths from "../paths";
+import * as path from "path";
+import * as fs from "fs";
+
+export default class CreateComponent extends Command {
+  static description = 'creates component $name $based-on';
+
+  static flags = {
+    help: flags.help({char: 'h'}),
+    force: flags.boolean({char: 'f'}),
+  }
+
+  protected basedOnOptions = ["class", "fn"];
+
+  static args = [{name: 'name'}, {name: "basedOn"}];
+
+  async run() {
+    // TODO
+    // HAVE TO LOAD DATA FROM CFG
+    const {args, flags} = this.parse(CreateComponent);
+    const componentPath = getComponentPath(args.name);
+
+    validateChooseArg(args.basedOn, this.basedOnOptions);
+
+    const templatePath = args.basedOn == "class"? paths.cTemplatePath : paths.fnTemplatePath;
+    const newComponentIndexPath = path.join(paths.componentsPath, path.basename(templatePath));
+
+    try {
+      await fs.promises.access(paths.componentsPath);
+    }
+
+    catch(e) {
+      if (e.code == "ENOENT") {
+        await fs.promises.mkdir(paths.componentsPath, { recursive: true });
+      }
+
+      console.error(e);
+      throw(e);
+    }
+
+    try {
+      await fs.promises.copyFile(templatePath, newComponentIndexPath, fs.constants.COPYFILE_FICLONE);
+    }
+
+    catch(e) {
+      console.error(e);
+      throw(e);
+    }
+  }
+}
