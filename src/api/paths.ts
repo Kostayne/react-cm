7b1930607226135b6ec22a2c1f4fb51f27c40204
kostayne <fuckogg@yandex.ru>
2021-03-02 22:02:24 +0300

    fixed working directory path (user path)

--- src/api/paths.ts
+++ src/api/paths.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 
-export const userPath = path.join(process.argv[1], "../", "../");
+export const userPath = path.join(process.cwd());
 export const configPath = path.join(userPath, "react-cm.json");
 export const packagePath = path.join(userPath, "package.json");
 export const componentsPath = path.join(userPath, "src", "templates");
